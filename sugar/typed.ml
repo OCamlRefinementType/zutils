c928d1aeeedb6e659009ec2c3e59572e3cd5b9b8
zhouzhezz <zorichzorich@gmail.com>
2024-09-08 19:45:48 -0400

    add aux for typed

--- sugar/typed.ml
+++ sugar/typed.ml
@@ -4,6 +4,8 @@ let ( #> ) f { x; ty } = { x = f x; ty }
 let ( #-> ) f { x; ty } = { x; ty = f ty }
 let ( #: ) x ty = { x; ty }
 let ( #+ ) x ty = { x = x.x; ty }
+let typed_from_pair (x, ty) = x #: ty
+let typed_to_pair { x; ty } = (x, ty)
 
 (** override show *)
 let show_typed (f : 'a -> string) (g : 'b -> string) { x; ty } =
