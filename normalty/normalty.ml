20e5520bc1d828223779a69d48e38c0e0f8473c4
zhouzhezz <zorichzorich@gmail.com>
2024-09-09 20:16:52 -0400

    add force typed

--- normalty/normalty.ml
+++ normalty/normalty.ml
@@ -8,7 +8,13 @@ include Subst
 (* module Frontend = Frontend *)
 (* module Connective = Connective *)
 include Frontend
+open Sugar
 
+let __force_known loc = function
+  | Ty_unknown -> _die_with loc "unkonwn type"
+  | _ as ty -> ty
+
+let __force_typed loc x = (__force_known loc) #=> x
 let __type_unify = Unification.__type_unify layout_nt
 let _type_unify = __type_unify
 let mk_arr a b = Ty_arrow (a, b)
@@ -16,9 +22,6 @@ let layout = layout_nt
 let layout_nt = layout_nt
 let nt_of_string = nt_of_string
 let string_of_nts = string_of_nts
-
-open Sugar
-
 let untyped x = { x; ty = Ty_unknown }
 let nt_name nt = String.concat "_" @@ String.split_on_char ' ' @@ layout_nt nt
 
