6eef55475382ff13e29ec24004784a98568be0ac
zhouzhezz <zorichzorich@gmail.com>
2024-10-10 18:29:16 -0400

    use new color print package

--- sugar/pp.ml
+++ sugar/pp.ml
@@ -3,5 +3,5 @@ open SugarAux
 let mk_colored color str = spf " @{<%s>%s@}" color str
 let mk_red = mk_colored "red"
 let mk_blue = mk_colored "blue"
-let printf = Ocolor_format.printf
-let printfBold boldstr str = Ocolor_format.printf "@{<bold>%s@}%s" boldstr str
+let printf = Spectrum.Simple.printf
+let printfBold boldstr str = Spectrum.Simple.printf "@{<bold>%s@}%s" boldstr str
