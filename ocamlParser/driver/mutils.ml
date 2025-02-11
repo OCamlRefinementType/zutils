93e5a08972b10e894d8fb265a012c89f241335af
zhouzhezz <zorichzorich@gmail.com>
2025-02-11 17:45:18 -0500

    update parser

--- ocamlParser/driver/mutils.ml
+++ ocamlParser/driver/mutils.ml
@@ -197,3 +197,20 @@ let mk_construct (name, args) =
 let mk_empty_list = mk_construct ("[]", [])
 let mk_list_cons_apply a b = mk_construct ("::", [ a; b ])
 let mk_list_literal l = List.fold_right mk_list_cons_apply l mk_empty_list
+
+let get_denoteopt_from_attr a =
+  match a with [ x ] -> Some x.attr_name.txt | _ -> None
+
+let get_denoteopt expr = get_denoteopt_from_attr expr.pexp_attributes
+
+let get_denote expr =
+  match get_denoteopt expr with
+  | Some x -> x
+  | None -> failwith "die::get_denote"
+
+let get_pat_denoteopt pat = get_denoteopt_from_attr pat.ppat_attributes
+
+let get_pat_denote expr =
+  match get_pat_denoteopt expr with
+  | Some x -> x
+  | None -> failwith "die::get_pat_denote"
