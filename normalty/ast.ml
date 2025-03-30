1e47d29c73ad34cc679f6e966ebb676048618c5c
zhezhouzz <zorichzorich@gmail.com>
2025-03-30 11:38:13 -0700

    add wf check

--- normalty/ast.ml
+++ normalty/ast.ml
@@ -88,6 +88,36 @@ let get_enum_elems = function
 let get_arr_lhs = function Ty_arrow (t1, _) -> t1 | _ -> _die [%here]
 let get_arr_rhs = function Ty_arrow (_, t2) -> t2 | _ -> _die [%here]
 
+open Zdatatype
+
+let wf_nt t =
+  let rec aux tvars = function
+    | Ty_var x -> List.exists (String.equal x) tvars
+    | Ty_any | Ty_unknown | Ty_uninter _ | Ty_enum _ -> true
+    | Ty_constructor (_, tps) -> List.for_all (aux tvars) tps
+    | Ty_record xs -> List.for_all (fun x -> aux tvars x.ty) xs
+    | Ty_arrow (nt1, nt2) -> List.for_all (aux tvars) [ nt1; nt2 ]
+    | Ty_tuple nts -> List.for_all (aux tvars) nts
+    | Ty_poly _ -> false
+  in
+  let rec aux_top tvars = function
+    | Ty_poly (x, ty) -> aux_top (x :: tvars) ty
+    | _ as ty -> aux tvars ty
+  in
+  aux_top [] t
+
+let gather_type_vars t =
+  let rec aux m = function
+    | Ty_var x -> StrMap.add x () m
+    | Ty_any | Ty_unknown | Ty_uninter _ | Ty_enum _ -> m
+    | Ty_constructor (_, tps) -> List.fold_left aux m tps
+    | Ty_record xs -> List.fold_left (fun m x -> aux m x.ty) m xs
+    | Ty_arrow (nt1, nt2) -> List.fold_left aux m [ nt1; nt2 ]
+    | Ty_tuple nts -> List.fold_left aux m nts
+    | Ty_poly (x, t) -> StrMap.remove x (aux m t)
+  in
+  StrMap.to_key_list @@ aux StrMap.empty t
+
 let has_poly_tp tp =
   let rec aux tp =
     match tp with
@@ -101,22 +131,30 @@ let has_poly_tp tp =
   aux tp
 
 let lift_poly_tp tp =
+  _assert [%here]
+    (spf "not a well-formed poly type: %s" (show_nt tp))
+    (wf_nt tp);
   let rec aux tp =
     match tp with
-    | Ty_unknown | Ty_any | Ty_var _ | Ty_uninter _ | Ty_enum _ -> ([], tp)
-    | Ty_constructor _ | Ty_record _ | Ty_tuple _ -> ([], tp)
-    | Ty_arrow (nt1, nt2) ->
-        let ps, nt2 = aux nt2 in
-        (ps, Ty_arrow (nt1, nt2))
     | Ty_poly (p, nt) ->
         let ps, nt = aux nt in
         (p :: ps, nt)
+    | _ -> ([], tp)
   in
   let ps, tp = aux tp in
   _assert [%here] "not a well-formed poly type" (not (has_poly_tp tp));
   _assert [%here] "same poly type var"
     (List.length ps == List.length (Zdatatype.List.slow_rm_dup String.equal ps));
   (ps, tp)
+
+let rec construct_poly_nt = function
+  | [], ty -> ty
+  | x :: xs, ty -> Ty_poly (x, construct_poly_nt (xs, ty))
+
+let close_poly_nt loc t =
+  let t = construct_poly_nt (gather_type_vars t, t) in
+  _assert loc (spf "not a well-formed poly type: %s" (show_nt t)) (wf_nt t);
+  t
 
 let destruct_arr_tp tp =
   let rec aux = function
