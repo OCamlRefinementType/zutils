deae67d99e3cdc0fef6688a49feab8c1a8889ed5
zhezhouzz <zorichzorich@gmail.com>
2025-04-05 15:38:12 -0700

    update rename

--- normalty/unification.ml
+++ normalty/unification.ml
@@ -4,7 +4,7 @@ open Zdatatype
 open Subst
 open Syntax
 
-(* let _fresh_type_var () = Ty_var (Rename.unique "__tvar") *)
+let _log = Myconfig._log "unification"
 
 module BoundConstraints = struct
   type bc = { type_vars : unit StrMap.t; cs : (t * t) list }
@@ -18,7 +18,7 @@ module BoundConstraints = struct
       | [] -> (vars, t)
       | p :: ps ->
           if exists vars p then (
-            let p' = Rename.unique p in
+            let p' = Rename.unique_type_var p in
             _assert [%here]
               (spf "rename success: %s in [%s]" p'
                  (StrList.to_string (StrMap.to_key_list vars)))
@@ -28,11 +28,12 @@ module BoundConstraints = struct
     in
     let res = aux vars (ps, t) in
     let () =
-      Pp.printf "@{<bold>uniquelize@}: %s :: %s ====>>===== %s :: %s\n"
-        (StrList.to_string (StrMap.to_key_list vars))
-        (layout_nt t)
-        (StrList.to_string (StrMap.to_key_list (fst res)))
-        (layout_nt (snd res))
+      _log (fun () ->
+          Pp.printf "@{<bold>uniquelize@}: %s :: %s ====>>===== %s :: %s\n"
+            (StrList.to_string (StrMap.to_key_list vars))
+            (layout_nt t)
+            (StrList.to_string (StrMap.to_key_list (fst res)))
+            (layout_nt (snd res)))
     in
     res
 
@@ -48,7 +49,7 @@ module BoundConstraints = struct
     ({ bc with cs = (t1, t2) :: bc.cs }, (t1, t2))
 
   let fresh { type_vars; cs } =
-    let x = Rename.unique "t" in
+    let x = Rename.fresh_type_var () in
     (* let () = Printf.printf "fresh: %s\n" x in *)
     _assert [%here] "fresh var create success" (not (exists type_vars x));
     ({ type_vars = StrMap.add x () type_vars; cs }, Ty_var x)
@@ -70,16 +71,15 @@ end
 let type_unification m (cs : (t * t) list) =
   let rec aux m cs =
     let () =
-      Pp.printf "@{<bold>m@}: %s\n"
-        (List.split_by_comma
-           (fun (x, ty) -> spf "%s := %s" x (layout_nt ty))
-           (StrMap.to_kv_list m))
-    in
-    let () =
-      Pp.printf "@{<bold>cs@}: %s\n"
-        (List.split_by_comma
-           (fun (x, ty) -> spf "%s = %s" (layout_nt x) (layout_nt ty))
-           cs)
+      _log (fun () ->
+          Pp.printf "@{<bold>m@}: %s\n"
+            (List.split_by_comma
+               (fun (x, ty) -> spf "%s := %s" x (layout_nt ty))
+               (StrMap.to_kv_list m));
+          Pp.printf "@{<bold>cs@}: %s\n"
+            (List.split_by_comma
+               (fun (x, ty) -> spf "%s = %s" (layout_nt x) (layout_nt ty))
+               cs))
     in
     match cs with
     | [] -> Some m
@@ -145,100 +145,3 @@ let unify_two_types loc (poly_vars : string list) (t1, t2) =
   match unify_two_types_opt poly_vars (t1, t2) with
   | None -> _die_with loc __FUNCTION__
   | Some t -> t
-
-(* open Zdatatype *)
-
-(* let __type_unify_ (pprint : t -> string) loc m t1 t2 = *)
-(*   (\* let () = Printf.printf "unify %s --> %s\n" (layout t1) (layout t2) in *\) *)
-(*   let rec unify m (t1, t2) = *)
-(*     let t1, t2 = map2 (msubst_nt m) (t1, t2) in *)
-(*     (\* let () = Printf.printf "one %s --> %s\n" (layout t1) (layout t2) in *\) *)
-(*     match (t1, t2) with *)
-(*     | Ty_unknown, Ty_unknown -> _die_with loc "unknown type" *)
-(*     | Ty_any, _ -> (m, t2) *)
-(*     | Ty_unknown, _ -> (m, t2) *)
-(*     | Ty_var n, t2 -> ( *)
-(*         match StrMap.find_opt m n with *)
-(*         | Some _ -> _die [%here] *)
-(*         | None -> (StrMap.add n t2 m, t2)) *)
-(*     | Ty_enum { enum_elems = []; _ }, Ty_enum { enum_elems = []; _ } -> *)
-(*         _die [%here] *)
-(*     | _, Ty_enum { enum_elems = []; _ } -> (m, t1) *)
-(*     | Ty_enum { enum_elems = []; _ }, _ -> (m, t2) *)
-(*     (\* | Ty_enum _, Ty_enum _ -> *\) *)
-(*     | Ty_constructor (id1, ts1), Ty_constructor (id2, ts2) -> *)
-(*         let id = _check_equality loc String.equal id1 id2 in *)
-(*         let m, ts = *)
-(*           List.fold_left *)
-(*             (fun (m, ts) (t1, t2) -> *)
-(*               let m, t = unify m (t1, t2) in *)
-(*               (m, ts @ [ t ])) *)
-(*             (m, []) (List.combine ts1 ts2) *)
-(*         in *)
-(*         (m, Ty_constructor (id, ts)) *)
-(*     | Ty_arrow (t11, t12), Ty_arrow (t21, t22) -> *)
-(*         let m, t1 = unify m (t11, t21) in *)
-(*         let m, t2 = unify m (t12, t22) in *)
-(*         (m, Ty_arrow (t1, t2)) *)
-(*     (\* unfold singleton tuple *\) *)
-(*     | Ty_tuple [ t1 ], _ -> unify m (t1, t2) *)
-(*     | _, Ty_tuple [ t2 ] -> unify m (t1, t2) *)
-(*     | Ty_tuple ts1, Ty_tuple ts2 when List.length ts1 == List.length ts2 -> *)
-(*         let m, ts = *)
-(*           List.fold_left *)
-(*             (fun (m, ts) (t1, t2) -> *)
-(*               let m, t = unify m (t1, t2) in *)
-(*               (m, ts @ [ t ])) *)
-(*             (m, []) (List.combine ts1 ts2) *)
-(*         in *)
-(*         (m, Ty_tuple ts) *)
-(*     | Ty_record l1, Ty_record l2 when List.length l1 == List.length l2 -> *)
-(*         let tab = Hashtbl.create (List.length l1) in *)
-(*         let () = List.iter (fun x -> Hashtbl.add tab x.x x.ty) l1 in *)
-(*         let m, l = *)
-(*           List.fold_left *)
-(*             (fun (m, l) x -> *)
-(*               match Hashtbl.find_opt tab x.x with *)
-(*               | None -> _die_with [%here] (spf "connot find feild %s" x.x) *)
-(*               | Some t' -> *)
-(*                   let m, t = unify m (t', x.ty) in *)
-(*                   (m, (x#+t) :: l)) *)
-(*             (m, []) l2 *)
-(*         in *)
-(*         (m, Ty_record l) *)
-(*     | _, Ty_any -> (m, t1) *)
-(*     | _, Ty_unknown -> (m, t1) *)
-(*     | _, Ty_var _ -> *)
-(*         (\* (m, t1) *\) *)
-(*         _die_with loc "argment should not contain type var" *)
-(*     | _, _ -> *)
-(*         ( m, *)
-(*           try _check_equality loc equal_nt t1 t2 *)
-(*           with e -> *)
-(*             Printf.printf "%s != %s\n" (show_nt t1) (show_nt t2); *)
-(*             raise e ) *)
-(*   in *)
-(*   try unify m (t1, t2) *)
-(*   with e -> *)
-(*     Printf.printf "Type unify error: %s ==> %s\n" (pprint t1) (pprint t2); *)
-(*     Printf.printf "Precisely: %s ==> %s\n" (show_nt t1) (show_nt t2); *)
-(*     raise e *)
-
-(* let __type_unify_v1 pprint loc t1 t2 = *)
-(*   match snd @@ __type_unify_ pprint loc StrMap.empty t1 t2 with *)
-(*   | Ty_unknown -> _die_with loc "still unknown type" *)
-(*   | _ as ty -> ty *)
-
-(* let __type_unify_v2 (pprint : t -> string) loc t1 t2 = *)
-(*   let m = type_unification_v2 StrMap.empty [ (t1, t2) ] in *)
-(*   let error_print () = *)
-(*     Printf.printf "Type unify error: %s ==> %s\n" (pprint t1) (pprint t2); *)
-(*     _die_with loc "normal type check error" *)
-(*   in *)
-(*   match m with *)
-(*   | Some m -> *)
-(*       let t1, t2 = map2 (msubst_nt m) (t1, t2) in *)
-(*       if equal_nt t1 t2 then t1 else error_print () *)
-(*   | None -> error_print () *)
-
-(* let __type_unify = __type_unify_v2 *)
--- sugar/rename.ml
+++ sugar/rename.ml
@@ -1,47 +1,45 @@
-let universe_label = ref 0
+open Assertion
+open SugarAux
 
-let name () =
-  let n = Printf.sprintf "_x%i" !universe_label in
-  universe_label := !universe_label + 1;
-  n
-
-let name_tab = Hashtbl.create 100
-
-open Printf
+type name = string * int option
 
 let split_char = '_'
 
-let unique_ name =
-  match Hashtbl.find_opt name_tab name with
+let name_of_string name =
+  match String.split_on_char split_char name with
+  | [ x ] -> (x, None)
+  | [ x; i ] -> (x, Some (int_of_string i))
+  | _ -> _die_with [%here] (Printf.sprintf "not a well-formed name: %s" name)
+
+let name_to_string (sname, id) =
+  match id with None -> sname | Some i -> spf "%s%c%i" sname split_char i
+
+let _unique tab sname =
+  match Hashtbl.find_opt tab sname with
   | Some n ->
-      Hashtbl.replace name_tab name (n + 1);
-      sprintf "%s%c%i" name split_char (n + 1)
+      Hashtbl.replace tab sname (n + 1);
+      (sname, Some n)
   | None ->
-      Hashtbl.add name_tab name 0;
-      sprintf "%s%c%i" name split_char 0
+      Hashtbl.add tab sname 0;
+      (sname, None)
 
-let unique name =
-  let prefix, name =
-    match String.split_on_char split_char name with
-    | [ x ] -> (None, x)
-    | [ x; _ ] -> (None, x)
-    | [ p; x; _ ] -> (Some p, x)
-    | _ -> failwith "die"
-  in
-  let name = unique_ name in
-  match prefix with
-  | None -> name
-  | Some p -> p ^ String.make 1 split_char ^ name
+let mk_unique tab name =
+  let sname, id = name_of_string name in
+  match id with
+  | Some _ ->
+      _die_with [%here]
+        (Printf.sprintf "the bound name (%s) cannot contain char %c" name
+           split_char)
+  | None -> name_to_string @@ _unique tab sname
 
-let dummy () = unique "dummyVar"
-
-let unique_with_prefix p name =
-  let _, name =
-    match String.split_on_char split_char name with
-    | [ x ] -> (None, x)
-    | [ x; _ ] -> (None, x)
-    | [ p; x; _ ] -> (Some p, x)
-    | _ -> failwith "die"
-  in
-  let name = unique_ name in
-  p ^ String.make 1 split_char ^ name
+(* NOTE: store the next available name lazily *)
+let universal_type_var : (string, int) Hashtbl.t = Hashtbl.create 100
+let universal_var : (string, int) Hashtbl.t = Hashtbl.create 1000
+let _unique_type_var sname = _unique universal_type_var sname
+let _unique_var sname = _unique universal_var sname
+let unique_type_var name = mk_unique universal_type_var name
+let unique_var name = mk_unique universal_var name
+let dummy_var () = unique_var "dummyVar"
+let dummy_type_var () = unique_var "dummyTypeVar"
+let fresh_type_var () = unique_var "tv"
+let fresh_var () = unique_var "tmp"
