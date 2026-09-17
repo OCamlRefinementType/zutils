526d61a799c256d22bb85d7a4cf73d609a1c98f4
Patrick LaFontaine <32135464+Pat-Lafon@users.noreply.github.com>
2026-09-16 22:46:48 -0400

    drop dead comments from find_first_poly_type_from_axiom
    The `(** instantiate_poly_axioms *)` header names a function the tree does not
    define, and `aux` carries a commented-out debug `Printf`.

--- prop/axiom.ml
+++ prop/axiom.ml
@@ -54,14 +54,10 @@ let%test "pred_extension is one pass, in either rule order" =
   ext chained [ "hd" ] = [ "hd"; "list_mem" ]
   && ext (List.rev chained) [ "hd" ] = [ "hd"; "list_mem" ]
 
-(** instantiate_poly_axioms *)
-
-(* The first type has poly var *)
 let find_first_poly_type_from_axiom prop =
   let rec aux prop =
     match prop with
     | Exists { body; qv } | Forall { body; qv } -> (
-        (* let () = Printf.printf "qv.ty : %s\n" (Nt.layout qv.ty) in *)
         match Nt.gather_type_vars qv.ty with
         | [ x ] when String.equal unified_axiom_type_var x -> Some qv
         | [] -> aux body
